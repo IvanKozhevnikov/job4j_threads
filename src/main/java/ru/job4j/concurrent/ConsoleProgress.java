eb6434bcc4414342af6b328d9635de587be90a95
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-08-05 16:12:10 +0300

    5. Прерывание нити [#1019  [#1019]]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new ConsoleProgress());
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        String[] process = {"-", "\\", "|", "/"};
+        while (!Thread.currentThread().isInterrupted()) {
+            for (var symbol : process) {
+                System.out.print("\r loading ... " + symbol);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
