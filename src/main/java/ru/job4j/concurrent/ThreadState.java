04073140f90686ad13c58099b7f70a8373c20bdb
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-08-08 13:00:58 +0300

    3. Состояние нити. [#229175 #434769]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -4,22 +4,20 @@ public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
                 () -> {
-
+                    System.out.println("First Thread");
                 }
         );
         Thread second = new Thread(
                 () -> {
-
+                    System.out.println("Second Thread");
                 }
         );
         System.out.println(first.getState());
-        first.start();
         System.out.println(second.getState());
+        first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState());
-        }
-        while (second.getState() != Thread.State.TERMINATED) {
             System.out.println(second.getState());
         }
         System.out.println(first.getState());
