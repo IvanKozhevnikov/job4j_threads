ffc299ea28edfef6fe48fb38731a9842272ce186
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-08-09 13:46:40 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    private synchronized String getContent(Predicate<Integer> filter) throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            int data;
+            while ((data = reader.read()) != -1) {
+                if (filter.test(data)) {
+                    output.append((char) data);
+                }
+            }
+        }
+        return output.toString();
+    }
+
+    public synchronized String getContentWithoutUnicode(Predicate<Integer> filter) throws IOException {
+        return getContent(data -> data < 0x80);
+    }
+
+    public synchronized String getContentWithUnicode(Predicate<Integer> filter) throws IOException {
+        return getContent(data -> true);
+    }
+}
